9301bac48dbb6df5174125e53d47e02d41787ef7
Palash <palash21gupta@gmail.com>
2023-11-24 02:13:09 +0530

    pertag patch

--- config.h
+++ config.h
@@ -82,6 +82,9 @@ static const char *termcmd[]  = { "kitty", NULL };
 static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
 static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
 static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *playpausecmd[] = { "playerctl", "play-pause", NULL };
+static const char *playnextcmd[] = { "playerctl", "next", NULL };
+static const char *playprevcmd[] = { "playerctl", "previous", NULL };
 //backlight control commands
 static const char *brupcmd[] = { "sudo", "xbacklight", "-perceived", "-inc", "5", NULL };
 static const char *brdowncmd[] = { "sudo", "xbacklight", "-perceived", "-dec", "5", NULL };
@@ -152,6 +155,9 @@ static const Key keys[] = {
     { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
     { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
     { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+    { 0, XF86XK_AudioPlay,        spawn, {.v = playpausecmd } },
+    { 0, XF86XK_AudioNext,        spawn, {.v = playnextcmd } },
+    { 0, XF86XK_AudioPrev,        spawn, {.v = playprevcmd } },
     { 0, XF86XK_MonBrightnessUp, spawn, {.v = brupcmd} },
     { 0, XF86XK_MonBrightnessDown, spawn, {.v = brdowncmd} },
 	TAGKEYS(                        XK_1,                      0)
